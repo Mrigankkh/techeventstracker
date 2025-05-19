6cefa302d826c1296e828693cd5e796f817a4a2b
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2025-05-18 20:53:33 -0400

    feat: added daily notificaiton email system

--- src/main/java/com/techevents/repository/EventRepository.java
+++ src/main/java/com/techevents/repository/EventRepository.java
@@ -1,10 +1,16 @@
 package com.techevents.repository;
 
 import com.techevents.model.Event;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    
+    List<Event> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 
 }
